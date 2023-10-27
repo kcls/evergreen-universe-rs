e343ff592b25710bc4a032f907a8a4c039e0f9ee
Bill Erickson <berickxx@gmail.com>
2023-10-27 13:16:59 -0400

    EG live circ tests
    Signed-off-by: Bill Erickson <berickxx@gmail.com>

--- evergreen/tests/live/circ.rs
+++ evergreen/tests/live/circ.rs
@@ -1,6 +1,7 @@
 use crate::util;
 use eg::common::circulator::Circulator;
 use eg::result::EgResult;
+use eg::constants as C;
 use evergreen as eg;
 use json;
 use std::collections::HashMap;
@@ -17,8 +18,12 @@ pub fn run_live_tests(tester: &mut util::Tester) -> EgResult<()> {
     tester.timer.stop("Created circ assets");
 
     tester.timer.start();
-    checkin_one(tester)?;
-    tester.timer.stop("checkin_one()");
+    checkin_item_at_home(tester)?;
+    tester.timer.stop("checkin_item_at_home()");
+
+    tester.timer.start();
+    checkin_item_remote(tester)?;
+    tester.timer.stop("checkin_item_remote()");
 
     tester.timer.start();
     delete_test_assets(tester)?;
@@ -51,7 +56,7 @@ fn delete_test_assets(tester: &mut util::Tester) -> EgResult<()> {
     e.commit()
 }
 
-fn checkin_one(tester: &mut util::Tester) -> EgResult<()> {
+fn checkin_item_at_home(tester: &mut util::Tester) -> EgResult<()> {
     let e = tester.editor.clone(); // circulator wants its own editor
 
     let mut options: HashMap<String, json::JsonValue> = HashMap::new();
@@ -75,10 +80,56 @@ fn checkin_one(tester: &mut util::Tester) -> EgResult<()> {
 
     assert!(evt.is_success());
 
+    let copy = &evt.payload()["copy"];
+
     assert_eq!(
-        evt.payload()["copy"]["barcode"].as_str(),
+        copy["barcode"].as_str(),
         Some(tester.samples.acp_barcode.as_str())
     );
+
+    assert_eq!(eg::util::json_int(&copy["status"])?, C::COPY_STATUS_RESHELVING);
+
+    Ok(())
+}
+
+fn checkin_item_remote(tester: &mut util::Tester) -> EgResult<()> {
+    let e = tester.editor.clone(); // circulator wants its own editor
+
+    let mut options: HashMap<String, json::JsonValue> = HashMap::new();
+    options.insert(
+        "copy_barcode".to_string(),
+        json::from(tester.samples.acp_barcode.to_string()),
+    );
+
+    // Tell the circulator we're operating from a different org unit.
+    options.insert(
+        "circ_lib".to_string(),
+        json::from(eg::samples::AOU_BR2_ID)
+    );
+
+    let mut circulator = Circulator::new(e, options)?;
+    circulator.begin()?;
+
+    // Collect needed data then kickoff the checkin process.
+    circulator.init().and_then(|()| circulator.checkin())?;
+
+    circulator.commit()?;
+
+    let evt = circulator
+        .events()
+        .get(0)
+        .ok_or(format!("Checkin returned no result!"))?;
+
+    assert_eq!(evt.textcode(), "ROUTE_ITEM");
+
+    let copy = &evt.payload()["copy"];
+
+    assert_eq!(
+        copy["barcode"].as_str(),
+        Some(tester.samples.acp_barcode.as_str())
+    );
+
+    assert_eq!(eg::util::json_int(&copy["status"])?, C::COPY_STATUS_IN_TRANSIT);
 
     Ok(())
 }
