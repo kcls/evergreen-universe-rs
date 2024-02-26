5f09e9de9b9e8c81da503f5c319da6fd54ce0bd4
Bill Erickson <berickxx@gmail.com>
2024-02-26 17:48:52 -0500

    cleanup some live tests
    Signed-off-by: Bill Erickson <berickxx@gmail.com>

--- evergreen/tests/live/json_query.rs
+++ evergreen/tests/live/json_query.rs
@@ -1,101 +1,22 @@
+//! Json Query Unit Tests
+//! NOTE Field names may change / be added that could affect these tests.
+//! Probably the test should be based more on the data returned from
+//! the queries than the than the compiled SQL.  This is a start.
 use crate::util;
 use eg::common::jq::JsonQueryCompiler;
 use eg::result::EgResult;
 use evergreen as eg;
-use json::JsonValue;
 
 pub fn run_live_tests(tester: &mut util::Tester) -> EgResult<()> {
-    let mut jq_compiler = JsonQueryCompiler::new(tester.ctx.idl().clone());
-
-    let query = json::object! {
-        "select": {
-            "acp": "*",
-            "acn": ["label", "owning_lib"],
-            "bre": "editor",
-            "acpl": "name",
-            "ccs": [{
-                "column": "name",
-                "alias": "status_label",
-                "transform": "uppercase",
-                //"params": [1, 2, 3], // TESTING
-            }]
-        },
-        "from": {
-            "acp": {
-                "acn": {
-                    "field": "id",
-                    "fkey": "call_number",
-                    "filter": {"record": 12345},
-                    "join": {
-                        "bre": {
-                            "fkey": "record"
-                        }
-                    }
-                },
-                "acpl": {
-                    "field": "id",
-                    "filter": {"holdable": "t", "deleted": "f"},
-                    "fkey": "location"
-                },
-                "ccs": {
-                    "field": "id",
-                    "filter": {"holdable": "t"},
-                    "fkey": "status"
-                },
-                "acpm": {
-                    "field": "target_copy",
-                    "type": "left"
-                }
-            }
-        },
-        "where": {
-            "+acp": {
-                "circulate": "t",
-                "deleted": "f",
-                "holdable": "t",
-                "circ_lib": {"not in": [1, 2, 3]},
-                "-or": [
-                    {"mint_condition": true},
-                    {"deposit": false}
-                ],
-                "id": {
-                    "not in": {
-                        "select": {"acp": ["id"]},
-                        "from": "acp",
-                        "where": {
-                            "editor": {"<>": 1}
-                        }
-                    }
-                }
-            },
-            "+acpm": {
-                "target_copy": JsonValue::Null
-            },
-            "+acn": {
-                "-or": [
-                   {"label": {"between": ["Hello", "Goodbye"]}},
-                   {"label": {"<>": "SUP"}}
-                ]
-            }
-        },
-        "order_by": [{
-            "class": "acp",
-            "field": "circ_lib",
-            "compare": {"!=" : {"+acn": "owning_lib"}}
-        }, {
-            "class": "acpl",
-            "field": "name"
-        }]
-    };
-
-    jq_compiler.compile(&query)?;
-    println!("\n{}\n", jq_compiler.debug_query_kludge());
-
     let query = json::object! {"order_by":[{"field":"id","class":"mfr"}],"select":{"mfr":["tag","value"]},"from":"mfr","where":{"record":"71","-or":[{"-and":[{"tag":"020"},{"subfield":"a"}]},{"-and":[{"tag":"022"},{"subfield":"a"}]},{"-and":[{"tag":"024"},{"subfield":"a"},{"ind1":1}]}]}};
 
-    jq_compiler = JsonQueryCompiler::new(tester.ctx.idl().clone());
+    let mut jq_compiler = JsonQueryCompiler::new(tester.ctx.idl().clone());
     jq_compiler.compile(&query)?;
-    println!("\n{}\n", jq_compiler.debug_query_kludge());
+
+    assert_eq!(
+        jq_compiler.debug_query_kludge(),
+        r#"SELECT "mfr".tag, "mfr".value FROM metabib.full_rec AS "mfr" WHERE "mfr".record = '71' AND (((("mfr".tag = '020') AND ("mfr".subfield = 'a'))) OR ((("mfr".tag = '022') AND ("mfr".subfield = 'a'))) OR ((("mfr".tag = '024') AND ("mfr".subfield = 'a') AND ("mfr".ind1 = '1')))) ORDER BY "mfr".id ASC"#
+    );
 
     let query = json::object! {"where":{"+ahr":{"-or":[{"-and":{"target":{"in":{"select":{"acp":["id"]},"from":{"acp":{"acn":{"field":"id","fkey":"call_number","join":{"bre":{"filter":{"id":32},"fkey":"record","field":"id"}}}}}}},"hold_type":["C","F","R"]}},{"-and":{"target":{"in":{"select":{"acn":["id"]},"from":{"acn":{"bre":{"field":"id","filter":{"id":32},"fkey":"record"}}}}},"hold_type":"V"}},{"-and":{"target":{"in":{"select":{"bmp":["id"]},"from":{"bmp":{"bre":{"fkey":"record","filter":{"id":32},"field":"id"}}}}},"hold_type":"P"}},{"-and":{"target":32,"hold_type":"T"}}],"cancel_time":null,"fulfillment_time":null}},"select":{"ahr":[{"column":"id","alias":"count","transform":"count"}]},"from":"ahr"};
 
@@ -116,7 +37,10 @@ pub fn run_live_tests(tester: &mut util::Tester) -> EgResult<()> {
     jq_compiler = JsonQueryCompiler::new(tester.ctx.idl().clone());
     jq_compiler.compile(&query)?;
 
-    println!("EXCLUDE\n{}", jq_compiler.debug_query_kludge());
+    assert_eq!(
+         jq_compiler.debug_query_kludge(),
+        r#"SELECT "bre".active, "bre".create_date, "bre".creator, "bre".deleted, "bre".edit_date, "bre".editor, "bre".fingerprint, "bre".id, "bre".last_xact_id, "bre".merge_date, "bre".merged_to, "bre".owner, "bre".quality, "bre".share_depth, "bre".source, "bre".tcn_source, "bre".tcn_value FROM biblio.record_entry AS "bre" WHERE ("bre".id BETWEEN 1 AND 10)"#
+    );
 
     Ok(())
 }
