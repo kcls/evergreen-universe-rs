a303bcf947e78a66384ee000402d876023700b4c
Bill Erickson <berickxx@gmail.com>
2024-04-04 16:59:18 -0400

    EG readme / editor examples
    Signed-off-by: Bill Erickson <berickxx@gmail.com>

--- evergreen/examples/editor.rs
+++ evergreen/examples/editor.rs
@@ -1,7 +1,39 @@
 use evergreen as eg;
 
-fn main() -> Result<(), String> {
+fn main() -> eg::EgResult<()> {
     let ctx = eg::init()?;
+    let mut editor = eg::Editor::new(ctx.client());
+
+    let orgs = editor.search("aou", eg::hash! {"id": {">": 0}})?;
+
+    for org in orgs {
+       println!("Org: {org}");
+    }
+
+    let mut new_org = eg::hash! {
+        "ou_type": 1,
+        "shortname": "TEST",
+        "name": "TEST NAME",
+    };
+
+	// Turn a bare hash into a blessed org unit ("aou") value.
+    new_org.bless("aou")?;
+
+    // Modify a value after instantiation
+    new_org["email"] = "home@example.org".into();
+
+    // Start a database transaction so we can modify data.
+    editor.xact_begin()?;
+
+    // Editor::create returns the newly created value.
+    new_org = editor.create(new_org)?;
+
+    println!("Add Org: {new_org} email={}", new_org["email"]);
+
+    // Rollback the transaction and disconnect
+    editor.rollback()?;
+
+
     let client = ctx.client();
 
     let args = eg::auth::AuthLoginArgs::new("br1mclark", "montyc1234", "temp", None);
