652c817982871eaa5ddc57b276e112e207eaf660
Bill Erickson <berickxx@gmail.com>
2024-09-18 14:55:54 -0400

    MARC export supports authorities; uses scripter
    Signed-off-by: Bill Erickson <berickxx@gmail.com>

--- evergreen/src/bin/marc-export.rs
+++ evergreen/src/bin/marc-export.rs
@@ -1,12 +1,92 @@
 use eg::date;
-use eg::db::DatabaseConnection;
+use eg::script::ScriptUtil;
 use evergreen as eg;
 use marc::Record;
 use postgres_cursor::Cursor;
 use rust_decimal::Decimal;
 use std::io::prelude::*;
 use std::path::Path;
-use std::{env, fs, io};
+use std::{fs, io};
+
+const HELP_TEXT: &str = r#"
+
+Synopsis
+
+    # Export all bib records as XML
+    eg-marc-export --to-xml --out-file /tmp/records.xml
+
+    # Export bib records that have holdings at BR1 including holdings.
+    eg-marc-export --to-xml --out-file /tmp/records.xml --items --library BR1
+
+Options
+
+    --authority-records
+        Export authority records instead of bibliographic records
+
+    --min-id <record-id>
+        Only export records whose ID is >= this value.
+
+    --max-id <record-id>
+        Only export records whose ID is <= this value.
+
+    --batch-size
+        Number of records to pull from the database per batch.  Batching
+        the records means not having to load every record into memory up
+        front before output writing can begin.
+
+    --out-file
+        Write data to this file.  Otherwise, writes to STDOUT.
+
+    --query-file
+        Path to a file containing an SQL query.  The query must produce
+        rows that have a columns named "id" and "marc".
+
+    --items
+        Includes holdings (copies / items) in the export.  Items are
+        added as MARC 852 fields.
+
+    --limit-to-opac-visible
+        Limits holdings (copies / items) in the export to those that
+        are visible in the OPAC. This option does nothing if the
+        --items option is not used.
+
+    --library <shortname>
+        Limit to records that have holdings at the specified library
+        by shortname.  Repeatable.
+
+    --force-ordered-holdings-fields
+        Insert holdings/items fields in tag order.  The default is
+        to append the fields to the end of the record, which is
+        generally faster.
+
+    --order-by-id
+        Sort data (records, etc.) by ID.
+        This is useful for comparing output data, but increases
+        the overhead of any SQL queries.
+
+    --modified-since <ISO date>
+        Export record modified on or after the provided date(time).
+        E.g. --modified-since 2023-10-12
+        E.g. --modified-since 2023-10-12T11:29:03-0400
+
+    --currency-symbol <symbol>
+        Money values (e.g. copy price) are preceded by this symbol.
+        Defaults to $.
+
+    --db-host <host>
+    --db-port <port>
+    --db-user <user>
+    --db-name <database>
+        Database connection options.  PG environment vars are used as
+        defaults when available.
+
+    --verbose
+        Print debug info to STDOUT.  This is not compatible with
+        printing record data to STDOUT.
+
+    --help Print help message
+
+"#;
 
 const XML_COLLECTION_HEADER: &str = r#"<collection xmlns="http://www.loc.gov/MARC21/slim">"#;
 const XML_COLLECTION_FOOTER: &str = "</collection>";
@@ -81,6 +161,9 @@ struct ExportOptions {
 
     order_by_id: bool,
 
+    /// If true we're exporting authority records instead of bibs.
+    authority_records: bool,
+
     query_file: Option<String>,
     verbose: bool,
 }
@@ -91,8 +174,7 @@ enum ExportDestination {
     File(String),
 }
 
-fn read_options() -> Option<(ExportOptions, DatabaseConnection)> {
-    let args: Vec<String> = env::args().collect();
+fn read_options() -> Option<(ExportOptions, ScriptUtil)> {
     let mut opts = getopts::Options::new();
 
     opts.optopt("", "min-id", "", "");
@@ -106,6 +188,7 @@ fn read_options() -> Option<(ExportOptions, DatabaseConnection)> {
 
     opts.optmulti("", "library", "", "");
 
+    opts.optflag("", "authority-records", "");
     opts.optflag("", "order-by-id", "");
     opts.optflag("", "pretty-print-xml", "");
     opts.optflag("", "force-ordered-holdings-fields", "");
@@ -116,24 +199,19 @@ fn read_options() -> Option<(ExportOptions, DatabaseConnection)> {
     opts.optflag("h", "help", "");
     opts.optflag("v", "verbose", "");
 
-    DatabaseConnection::append_options(&mut opts);
+    let scripter =
+        match ScriptUtil::init(&mut opts, true, Some(HELP_TEXT)).expect("Scripter should init()") {
+            Some(s) => s,
+            None => return None,
+        };
 
-    let params = opts.parse(&args[1..]).unwrap();
-
-    if params.opt_present("help") {
-        print_help();
-        return None;
-    }
-
-    let destination = match params.opt_get::<String>("out-file").unwrap() {
+    let destination = match scripter.params().opt_get::<String>("out-file").unwrap() {
         Some(filename) => ExportDestination::File(filename),
         None => ExportDestination::Stdout,
     };
 
-    let connection = DatabaseConnection::new_from_options(&params);
-
     let mut modified_since = None;
-    if let Some(mod_since) = params.opt_str("modified-since") {
+    if let Some(mod_since) = scripter.params().opt_str("modified-since") {
         match date::parse_datetime(&mod_since) {
             Ok(d) => modified_since = Some(date::to_iso(&d)),
             Err(e) => {
@@ -143,7 +221,10 @@ fn read_options() -> Option<(ExportOptions, DatabaseConnection)> {
         }
     }
 
-    let batch_size = match params.opt_get_default("batch-size", DEFAULT_BATCH_SIZE) {
+    let batch_size = match scripter
+        .params()
+        .opt_get_default("batch-size", DEFAULT_BATCH_SIZE)
+    {
         Ok(s) => {
             if s == 0 {
                 DEFAULT_BATCH_SIZE
@@ -161,108 +242,31 @@ fn read_options() -> Option<(ExportOptions, DatabaseConnection)> {
         destination,
         modified_since,
         batch_size,
-        pipe: params.opt_present("pipe"),
+        pipe: scripter.params().opt_present("pipe"),
         record_ids: None,
-        pretty_print_xml: params.opt_present("pretty-print-xml"),
-        min_id: params.opt_get_default("min-id", -1).unwrap(),
-        max_id: params.opt_get_default("max-id", -1).unwrap(),
-        location_code: params.opt_str("location-code"),
-        libraries: params.opt_strs("library"),
+        pretty_print_xml: scripter.params().opt_present("pretty-print-xml"),
+        min_id: scripter.params().opt_get_default("min-id", -1).unwrap(),
+        max_id: scripter.params().opt_get_default("max-id", -1).unwrap(),
+        location_code: scripter.params().opt_str("location-code"),
+        libraries: scripter.params().opt_strs("library"),
         library_ids: None,
-        currency_symbol: params
+        authority_records: scripter.params().opt_present("authority-records"),
+        currency_symbol: scripter
+            .params()
             .opt_get_default("currency-symbol", "$".to_string())
             .unwrap(),
-        order_by_id: params.opt_present("order-by-id"),
-        force_ordered_holdings_fields: params.opt_present("force-ordered-holdings-fields"),
-        export_items: params.opt_present("items"),
-        limit_to_visible: params.opt_present("limit-to-opac-visible"),
-        verbose: params.opt_present("verbose"),
-        to_xml: params.opt_present("to-xml"),
-        query_file: params.opt_get("query-file").unwrap(),
+        order_by_id: scripter.params().opt_present("order-by-id"),
+        force_ordered_holdings_fields: scripter
+            .params()
+            .opt_present("force-ordered-holdings-fields"),
+        export_items: scripter.params().opt_present("items"),
+        limit_to_visible: scripter.params().opt_present("limit-to-opac-visible"),
+        verbose: scripter.params().opt_present("verbose"),
+        to_xml: scripter.params().opt_present("to-xml"),
+        query_file: scripter.params().opt_get("query-file").unwrap(),
     };
 
-    Some((options, connection))
-}
-
-fn print_help() {
-    println!(
-        r#"
-
-Synopsis
-
-    # Export all bib records as XML
-    eg-marc-export --to-xml --out-file /tmp/records.xml
-
-    # Export bib records that have holdings at BR1 including holdings.
-    eg-marc-export --to-xml --out-file /tmp/records.xml --items --library BR1
-
-Options
-
-    --min-id <record-id>
-        Only export records whose ID is >= this value.
-
-    --max-id <record-id>
-        Only export records whose ID is <= this value.
-
-    --batch-size
-        Number of records to pull from the database per batch.  Batching
-        the records means not having to load every record into memory up
-        front before output writing can begin.
-
-    --out-file
-        Write data to this file.  Otherwise, writes to STDOUT.
-
-    --query-file
-        Path to a file containing an SQL query.  The query must produce
-        rows that have a columns named "id" and "marc".
-
-    --items
-        Includes holdings (copies / items) in the export.  Items are
-        added as MARC 852 fields.
-
-    --limit-to-opac-visible
-        Limits holdings (copies / items) in the export to those that
-        are visible in the OPAC. This option does nothing if the
-        --items option is not used.
-
-    --library <shortname>
-        Limit to records that have holdings at the specified library
-        by shortname.  Repeatable.
-
-    --force-ordered-holdings-fields
-        Insert holdings/items fields in tag order.  The default is
-        to append the fields to the end of the record, which is
-        generally faster.
-
-    --order-by-id
-        Sort data (records, etc.) by ID.
-        This is useful for comparing output data, but increases
-        the overhead of any SQL queries.
-
-    --modified-since <ISO date>
-        Export record modified on or after the provided date(time).
-        E.g. --modified-since 2023-10-12
-        E.g. --modified-since 2023-10-12T11:29:03-0400
-
-    --currency-symbol <symbol>
-        Money values (e.g. copy price) are preceded by this symbol.
-        Defaults to $.
-
-    --db-host <host>
-    --db-port <port>
-    --db-user <user>
-    --db-name <database>
-        Database connection options.  PG environment vars are used as
-        defaults when available.
-
-    --verbose
-        Print debug info to STDOUT.  This is not compatible with
-        printing record data to STDOUT.
-
-    --help Print help message
-
-    "#
-    );
+    Some((options, scripter))
 }
 
 fn create_records_sql(ops: &ExportOptions) -> String {
@@ -270,46 +274,53 @@ fn create_records_sql(ops: &ExportOptions) -> String {
         return fs::read_to_string(fname).unwrap();
     }
 
-    let select = "SELECT DISTINCT bre.id, bre.marc";
-    let mut from = "FROM biblio.record_entry bre".to_string();
+    let select = "SELECT DISTINCT rec.id, rec.marc";
+
+    let mut from = if ops.authority_records {
+        "FROM authority.record_entry rec".to_string()
+    } else {
+        "FROM biblio.record_entry rec".to_string()
+    };
 
     // Also check for presence of at least one copy and/or URI?
     if let Some(ids) = ops.library_ids.as_ref() {
-        from += &format!(
-            r#"
-            JOIN asset.call_number acn ON (
-                acn.record = bre.id
-                AND acn.owning_lib IN ({ids})
-                AND NOT acn.deleted
-            )
-        "#
-        );
+        if !ops.authority_records {
+            from += &format!(
+                r#"
+                JOIN asset.call_number acn ON (
+                    acn.record = rec.id
+                    AND acn.owning_lib IN ({ids})
+                    AND NOT acn.deleted
+                )
+            "#
+            );
+        }
     }
 
-    let mut filter = String::from("WHERE NOT bre.deleted");
+    let mut filter = String::from("WHERE NOT rec.deleted");
 
     if ops.min_id > -1 {
-        filter += &format!(" AND bre.id >= {}", ops.min_id);
+        filter += &format!(" AND rec.id >= {}", ops.min_id);
     }
 
     if ops.max_id > -1 {
-        filter += &format!(" AND bre.id < {}", ops.max_id);
+        filter += &format!(" AND rec.id < {}", ops.max_id);
     }
 
     if let Some(record_ids) = ops.record_ids.as_ref() {
-        filter += &format!(" AND bre.id in ({record_ids})");
+        filter += &format!(" AND rec.id in ({record_ids})");
     }
 
     if let Some(since) = ops.modified_since.as_ref() {
         // edit_date is set at create time, so there's no
         // need to additionally check create_date.
-        filter += &format!(" AND bre.edit_date >= '{since}'");
+        filter += &format!(" AND rec.edit_date >= '{since}'");
     }
 
     let mut sql = format!("{select} {from} {filter}");
 
     if ops.order_by_id {
-        sql += " ORDER BY bre.id";
+        sql += " ORDER BY rec.id";
     }
 
     sql
@@ -407,7 +418,7 @@ fn set_pipe_ids(ops: &mut ExportOptions) -> Result<(), String> {
 }
 
 /// Translate library filter shortnames into org unit IDs
-fn set_library_ids(con: &mut DatabaseConnection, ops: &mut ExportOptions) -> Result<(), String> {
+fn set_library_ids(scripter: &mut ScriptUtil, ops: &mut ExportOptions) -> Result<(), String> {
     if ops.libraries.is_empty() {
         return Ok(());
     }
@@ -415,7 +426,12 @@ fn set_library_ids(con: &mut DatabaseConnection, ops: &mut ExportOptions) -> Res
     let mut ids = String::new();
     let query = "select id from actor.org_unit where shortname=any($1::text[])";
 
-    for row in con.client().query(query, &[&ops.libraries]).unwrap() {
+    for row in scripter
+        .db()
+        .client()
+        .query(query, &[&ops.libraries])
+        .unwrap()
+    {
         ids += &format!("{},", row.get::<&str, i32>("id"));
     }
 
@@ -425,7 +441,7 @@ fn set_library_ids(con: &mut DatabaseConnection, ops: &mut ExportOptions) -> Res
     Ok(())
 }
 
-fn export(con: &mut DatabaseConnection, ops: &mut ExportOptions) -> Result<(), String> {
+fn export(scripter: &mut ScriptUtil, ops: &mut ExportOptions) -> Result<(), String> {
     // Where are we spewing bytes?
     let mut writer: Box<dyn Write> = match &ops.destination {
         ExportDestination::File(fname) => {
@@ -437,9 +453,7 @@ fn export(con: &mut DatabaseConnection, ops: &mut ExportOptions) -> Result<(), S
         _ => Box::new(io::stdout()),
     };
 
-    con.connect()?;
-
-    set_library_ids(con, ops)?;
+    set_library_ids(scripter, ops)?;
     set_pipe_ids(ops)?;
 
     if ops.to_xml {
@@ -458,10 +472,9 @@ fn export(con: &mut DatabaseConnection, ops: &mut ExportOptions) -> Result<(), S
         println!("Record batch SQL:\n{query}");
     }
 
-    // A cursor is by definition a long running mutable thing.  Create a
-    // separate connection so we can leave the cursor open and running
-    // while doing other DB stuff with the main connection.
-    let mut cursor_con = con.clone();
+    // Create a separate DB connection for our long-running mutable
+    // cursor so can perform other DB actions in parallel
+    let mut cursor_con = scripter.db().clone();
     cursor_con.connect()?;
 
     let mut cursor = Cursor::build(cursor_con.client())
@@ -491,7 +504,7 @@ fn export(con: &mut DatabaseConnection, ops: &mut ExportOptions) -> Result<(), S
             };
 
             if let Some(items_sql) = &items_query {
-                add_items(record_id, con, ops, &mut record, items_sql)?;
+                add_items(record_id, scripter, ops, &mut record, items_sql)?;
             }
 
             if ops.to_xml {
@@ -543,14 +556,19 @@ fn export(con: &mut DatabaseConnection, ops: &mut ExportOptions) -> Result<(), S
 /// Append holdings data to this MARC record.
 fn add_items(
     record_id: i64,
-    con: &mut DatabaseConnection,
+    scripter: &mut ScriptUtil,
     ops: &ExportOptions,
     record: &mut Record,
     items_query: &str,
 ) -> Result<(), String> {
     record.remove_fields(HOLDINGS_SUBFIELD);
 
-    for row in con.client().query(items_query, &[&record_id]).unwrap() {
+    for row in scripter
+        .db()
+        .client()
+        .query(items_query, &[&record_id])
+        .unwrap()
+    {
         let mut field = marc::Field::new(HOLDINGS_SUBFIELD)?;
         field.set_ind1("4")?;
 
@@ -622,9 +640,9 @@ fn check_options(ops: &ExportOptions) -> Result<(), String> {
 }
 
 fn main() -> Result<(), String> {
-    if let Some((mut options, mut connection)) = read_options() {
+    if let Some((mut options, mut scripter)) = read_options() {
         check_options(&options)?;
-        export(&mut connection, &mut options)
+        export(&mut scripter, &mut options)
     } else {
         Ok(())
     }
