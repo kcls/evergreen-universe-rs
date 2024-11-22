9c4beece8a4bd772099fd513431629d23c56e9a2
Bill Erickson <berickxx@gmail.com>
2024-11-22 11:11:31 -0500

    Make MARC namespace consts public
    Signed-off-by: Bill Erickson <berickxx@gmail.com>

--- marc/src/lib.rs
+++ marc/src/lib.rs
@@ -4,6 +4,9 @@ pub use self::record::Controlfield;
 pub use self::record::Field;
 pub use self::record::Record;
 pub use self::record::Subfield;
+pub use self::xml::MARCXML_NAMESPACE;
+pub use self::xml::MARCXML_XSI_NAMESPACE;
+pub use self::xml::MARCXML_SCHEMA_LOCATION;
 
 pub mod binary;
 pub mod breaker;
--- marc/src/xml.rs
+++ marc/src/xml.rs
@@ -9,9 +9,9 @@ use super::Field;
 use super::Record;
 use super::Subfield;
 
-const MARCXML_NAMESPACE: &str = "http://www.loc.gov/MARC21/slim";
-const MARCXML_XSI_NAMESPACE: &str = "http://www.w3.org/2001/XMLSchema-instance";
-const MARCXML_SCHEMA_LOCATION: &str =
+pub const MARCXML_NAMESPACE: &str = "http://www.loc.gov/MARC21/slim";
+pub const MARCXML_XSI_NAMESPACE: &str = "http://www.w3.org/2001/XMLSchema-instance";
+pub const MARCXML_SCHEMA_LOCATION: &str =
     "http://www.loc.gov/MARC21/slim http://www.loc.gov/standards/marcxml/schema/MARC21slim.xsd";
 
 /// Replace non-ASCII characters and special characters with escaped
