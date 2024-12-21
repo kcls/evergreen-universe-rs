e1e5307936b3b0767be922c65f5f76521e7678b6
Jane Sandberg <sandbergja@gmail.com>
2024-12-20 17:28:08 -0800

    [marctk] extract fields using an inclusive range or spec string
    Helps with #21
    
    Signed-off-by: Jane Sandberg <sandbergja@gmail.com>

---
+++ marctk/src/query.rs
@@ -0,0 +1,67 @@
+use std::ops::RangeInclusive;
+
+use crate::Field;
+
+pub struct Query {
+    pub field_filter: Box<dyn Fn(&&Field) -> bool>,
+}
+
+impl From<RangeInclusive<i64>> for Query {
+    fn from(range: RangeInclusive<i64>) -> Self {
+        Query {
+            field_filter: Box::new(move |f: &&Field| match f.tag().parse::<i64>() {
+                Ok(tag_number) => range.contains(&tag_number),
+                Err(_) => false,
+            }),
+        }
+    }
+}
+
+impl From<&str> for Query {
+    fn from(specification: &str) -> Self {
+        let spec = specification.to_owned();
+        Query {
+            field_filter: Box::new(move |f: &&Field| f.matches_spec(&spec)),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::Record;
+
+    #[test]
+    fn test_can_filter_by_inclusive_range() {
+        let query = Query::from(600..=699);
+        let record = Record::from_breaker(
+            r#"=600 10$aZhang, Heng, $d 78-139 $v Juvenile literature.
+=650 \0$aEarthquakes $v Juvenile literature.
+=955 \0$a1234"#,
+        )
+        .unwrap();
+        let filter = query.field_filter;
+        let mut filtered = record.fields().iter().filter(filter);
+        assert_eq!(filtered.next().unwrap().tag(), "600");
+        assert_eq!(filtered.next().unwrap().tag(), "650");
+        assert!(filtered.next().is_none());
+    }
+
+    #[test]
+    fn test_can_filter_by_string_slice() {
+        let query = Query::from("x50");
+        let record = Record::from_breaker(
+            r#"=150 \\$aLion
+=450 \\$aPanthera leo
+=550 \\$wg$aPanthera
+=953 \\$axx00$bec11"#,
+        )
+        .unwrap();
+        let filter = query.field_filter;
+        let mut filtered = record.fields().iter().filter(filter);
+        assert_eq!(filtered.next().unwrap().tag(), "150");
+        assert_eq!(filtered.next().unwrap().tag(), "450");
+        assert_eq!(filtered.next().unwrap().tag(), "550");
+        assert!(filtered.next().is_none());
+    }
+}
