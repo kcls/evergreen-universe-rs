e03f509401251e4413d79368874cedefb695526e
Bill Erickson <berickxx@gmail.com>
2025-01-21 16:16:38 -0500

    z39 server
    Signed-off-by: Bill Erickson <berickxx@gmail.com>

---
+++ z39-server/src/main.rs
@@ -0,0 +1,222 @@
+use evergreen as eg;
+
+use std::any::Any;
+use std::io::Read; // needed by TcpStream
+use std::net::{TcpListener, TcpStream};
+
+mod message;
+use message::Message;
+
+const BUFSIZE: usize = 1024;
+
+struct Z39ConnectRequest {
+    tcp_stream: Option<TcpStream>,
+}
+
+impl Z39ConnectRequest {
+    pub fn downcast(h: &mut Box<dyn mptc::Request>) -> &mut Z39ConnectRequest {
+        h.as_any_mut()
+            .downcast_mut::<Z39ConnectRequest>()
+            .expect("Z39ConnectRequest::downcast() given wrong type!")
+    }
+}
+
+impl mptc::Request for Z39ConnectRequest {
+    fn as_any_mut(&mut self) -> &mut dyn Any {
+        self
+    }
+}
+
+#[derive(Debug)]
+struct Z39Session {
+    id: u64,
+    tcp_stream: Option<TcpStream>,
+}
+
+impl Z39Session {
+
+    /// Panics if the stream is None.
+    fn tcp_stream_mut(&mut self) -> &mut TcpStream {
+        self.tcp_stream.as_mut().unwrap()
+    }
+
+    fn handle_message(&mut self, message: Message) -> Result<(), String> {
+        println!("REQ: {message:?}");
+
+        Ok(())
+    }
+}
+
+impl mptc::RequestHandler for Z39Session {
+    fn worker_start(&mut self) -> Result<(), String> {
+        println!("Z39Session::worker_start({})", self.id);
+        Ok(())
+    }
+
+    fn worker_end(&mut self) -> Result<(), String> {
+        println!("Z39Session::worker_end({})", self.id);
+        Ok(())
+    }
+
+    fn process(&mut self, mut request: Box<dyn mptc::Request>) -> Result<(), String> {
+        println!("Z39Session::process({})", self.id);
+
+        // Turn the general mptc::Request into a type we can perform actions on.
+        let request = Z39ConnectRequest::downcast(&mut request);
+
+        // Z39ConnectRequest's only real job was to pass us the stream.
+        self.tcp_stream = request.tcp_stream.take();
+
+        let mut bytes = Vec::new();
+        let mut buffer = [0u8; BUFSIZE];
+
+        // Read bytes from the TCP stream, feeding them into the BER
+        // parser, until a complete object/message is formed.  Handle
+        // the message, rinse and repeat.
+        while let Ok(count) = self.tcp_stream_mut().read(&mut buffer) {
+            if count == 0 {
+                log::debug!("client socket shutdown.  exiting");
+                break;
+            }
+
+            bytes.extend_from_slice(&buffer);
+
+            match Message::from_bytes(&bytes) {
+                Ok(op) => if let Some(msg) = op {
+                    self.handle_message(msg)?; // TODO
+                } else {
+                    // More bytes needed.
+                    continue;
+                }
+                Err(e) => {
+                    log::error!("Cannot parse message: {e} {bytes:?}");
+                    break;
+                }
+            }
+
+            //let Some(message) = Message::from_bytes(&bytes)
+
+            /*
+
+            match parse_ber(&bytes) {
+                Ok((rem, obj)) => {
+                    log::debug!("read {} bytes\n{bytes:?}\n", bytes.len());
+
+                    let message = match Message::from_ber(&obj) {
+                        Ok(m) => m,
+                        Err(e) => {
+                            log::error!("could not parse bytes as BER: {e:?} bytes={bytes:?}");
+                            break;
+                        }
+                    };
+
+                    if let Err(e) = self.handle_message(message) {
+                        log::warn!("exiting session after error: {e}");
+                        break;
+                    }
+
+                    // If we have trailing bytes add them to the pile
+                    // to get re-parsed on the next cycle.
+                    bytes = rem.to_vec();
+                }
+                Err(e) => {
+                    if let der_parser::asn1_rs::Err::Incomplete(_) = e {
+                        continue; // More data needed.
+                    } else {
+                        log::error!("parsing failed: {e:?} bytes={bytes:?}");
+                        break;
+                    }
+                }
+            }
+            */
+        }
+
+        /*
+        request
+            .tcp_stream
+            .write_all(format!("Replying from {:?}: ", std::thread::current().id()).as_bytes())
+            .expect("Stream.write()");
+
+        request
+            .tcp_stream
+            .write_all(&buffer[..count])
+            .expect("Stream.write()");
+        */
+
+        self.tcp_stream_mut().shutdown(std::net::Shutdown::Both).ok();
+
+        Ok(())
+    }
+}
+
+struct Z39Server {
+    tcp_listener: TcpListener,
+    id_gen: u64,
+}
+
+impl mptc::RequestStream for Z39Server {
+    fn next(&mut self) -> Result<Option<Box<dyn mptc::Request>>, String> {
+        let tcp_stream = match self.tcp_listener.accept() {
+            Ok((stream, addr)) => {
+                println!("z39 connect from {addr}");
+                stream
+            }
+            Err(e) => {
+                match e.kind() {
+                    std::io::ErrorKind::WouldBlock => {
+                        // See if we need to to into/out of ready mode.
+
+                        // TODO
+                        //self.check_heartbeat_signals();
+
+                        // No connection received within the timeout.
+                        // Return None to the mptc::Server so it can
+                        // perform housekeeping.
+                        return Ok(None);
+                    }
+                    _ => {
+                        log::error!("SIPServer accept() failed {e}");
+                        return Ok(None);
+                    }
+                }
+            }
+        };
+
+        let request = Z39ConnectRequest { tcp_stream: Some(tcp_stream) };
+        Ok(Some(Box::new(request)))
+    }
+
+    fn new_handler(&mut self) -> Box<dyn mptc::RequestHandler> {
+        self.id_gen += 1;
+        let h = Z39Session {
+            id: self.id_gen,
+            tcp_stream: None
+        };
+        Box::new(h)
+    }
+
+    fn reload(&mut self) -> Result<(), String> {
+        Ok(())
+    }
+
+    fn shutdown(&mut self) {}
+}
+
+fn main() {
+    let tcp_listener = eg::util::tcp_listener(
+        "127.0.0.1",
+        2210,
+        3, // TODO
+    ).unwrap(); //  todo
+
+    let server = Z39Server {
+        id_gen: 0,
+        tcp_listener,
+    };
+
+    let mut s = mptc::Server::new(Box::new(server));
+
+    s.run();
+}
+
+
---
+++ z39-server/src/message.rs
@@ -0,0 +1,190 @@
+use rasn::AsnType;
+use rasn::prelude::*;
+use rasn::ber::de::DecodeErrorKind;
+
+/// Translate an ASN GeneralString into a Rust &str.
+///
+pub fn general_string_to_str(g: &GeneralString) -> Result<&str, String> {
+    std::str::from_utf8(g.as_bytes()).map_err(|e| e.to_string())
+}
+
+#[derive(Debug, Default, AsnType, Decode, Encode)]
+#[rasn(tag(context, 20))]
+pub struct InitializeRequest {
+    #[rasn(tag(context, 2))]
+    pub reference_id: Option<OctetString>,
+    #[rasn(tag(context, 3))]
+    pub protocol_version: BitString,
+    #[rasn(tag(context, 4))]
+    pub options: BitString,
+    #[rasn(tag(context, 5))]
+    pub preferred_message_size: u32,
+    #[rasn(tag(context, 6))]
+    pub exceptional_record_size: u32,
+    #[rasn(tag(context, 110))]
+    pub implementation_id: Option<GeneralString>,
+    #[rasn(tag(context, 111))]
+    pub implementation_name: Option<GeneralString>,
+    #[rasn(tag(context, 112))]
+    pub implementation_version: Option<GeneralString>,
+}
+
+#[derive(Debug, Default, AsnType, Decode, Encode)]
+#[rasn(tag(context, 21))]
+pub struct InitializeResponse {
+    #[rasn(tag(context, 2))]
+    pub reference_id: Option<OctetString>,
+    #[rasn(tag(context, 3))]
+    pub protocol_version: BitString,
+    #[rasn(tag(context, 4))]
+    pub options: BitString,
+    #[rasn(tag(context, 5))]
+    pub preferred_message_size: u32,
+    #[rasn(tag(context, 6))]
+    pub exceptional_record_size: u32,
+    #[rasn(tag(12))]
+    pub result: Option<bool>,
+    #[rasn(tag(context, 110))]
+    pub implementation_id: Option<GeneralString>,
+    #[rasn(tag(context, 111))]
+    pub implementation_name: Option<GeneralString>,
+    #[rasn(tag(context, 112))]
+    pub implementation_version: Option<GeneralString>,
+}
+
+#[derive(Debug)]
+pub enum MessagePayload {
+    InitializeRequest(InitializeRequest),
+    InitializeResponse(InitializeResponse),
+}
+
+#[derive(Debug)]
+pub struct Message {
+    payload: MessagePayload
+}
+
+impl Message {
+    /// Parses a collection of bytes into a Message.
+    ///
+    /// Returns None if more bytes are needed to complete the message.
+    pub fn from_bytes(bytes: &[u8]) -> Result<Option<Self>, String> {
+        if bytes.is_empty() {
+            return Ok(None);
+        }
+
+        let payload = match format!("{:b}", &bytes[0]).as_str() {
+            // TODO matching on the binary representation of the first byte
+            // is hacky.  Parse the bits for real.
+
+            // The tag component are the final 5 bits, 10100=20 in this case.
+            "10110100" => { // Tag(20)
+                let msg: InitializeRequest = match rasn::ber::decode(&bytes) {
+                    Ok(m) => m,
+                    Err(e) => {
+                        match *e.kind {
+                            DecodeErrorKind::Incomplete { needed: _ } => return Ok(None),
+                            _ => return Err(e.to_string()),
+                        }
+                    }
+                };
+
+                MessagePayload::InitializeRequest(msg)
+            }
+            "10110101" => {  // Tag(21)
+                let msg: InitializeResponse = match rasn::ber::decode(&bytes) {
+                    Ok(m) => m,
+                    Err(e) => {
+                        match *e.kind {
+                            DecodeErrorKind::Incomplete { needed: _ } => return Ok(None),
+                            _ => return Err(e.to_string()),
+                        }
+                    }
+                };
+
+                MessagePayload::InitializeResponse(msg)
+            }
+
+            _ => todo!(),
+        };
+
+        Ok(Some(Message { payload }))
+    }
+
+    /// Translate a message into a collection of bytes suitable for delivery.
+    pub fn to_bytes(&self) -> Result<Vec<u8>, String> {
+        let res = match &self.payload {
+            MessagePayload::InitializeRequest(m) => rasn::ber::encode(&m),
+            MessagePayload::InitializeResponse(m) => rasn::ber::encode(&m),
+        };
+
+        res.map_err(|e| e.to_string())
+    }
+}
+
+
+#[test]
+fn test_encode_decode() {
+
+    // Example InitializeRequest from YAZ client.
+    let init_req_bytes = [
+        0xb4, 0x52, 0x83, 0x02, 0x00, 0xe0, 0x84, 0x03, 0x00, 0xe9,
+        0xa2, 0x85, 0x04, 0x04, 0x00, 0x00, 0x00, 0x86, 0x04, 0x04,
+        0x00, 0x00, 0x00, 0x9f, 0x6e, 0x02, 0x38, 0x31, 0x9f, 0x6f,
+        0x03, 0x59, 0x41, 0x5a, 0x9f, 0x70, 0x2f, 0x35, 0x2e, 0x33,
+        0x31, 0x2e, 0x31, 0x20, 0x63, 0x33, 0x63, 0x65, 0x61, 0x38,
+        0x38, 0x31, 0x65, 0x33, 0x65, 0x37, 0x65, 0x38, 0x30, 0x62,
+        0x30, 0x36, 0x39, 0x64, 0x64, 0x64, 0x31, 0x34, 0x32, 0x39,
+        0x39, 0x39, 0x34, 0x65, 0x35, 0x38, 0x38, 0x34, 0x31, 0x61,
+        0x63, 0x62, 0x31, 0x34
+    ];
+
+    let init_req_msg = Message::from_bytes(&init_req_bytes).unwrap().unwrap();
+
+    let MessagePayload::InitializeRequest(init_req) = &init_req_msg.payload else {
+        panic!("Wrong message type parsed: {init_req_msg:?}");
+    };
+
+    assert_eq!(
+        "YAZ", 
+        general_string_to_str(init_req.implementation_name.as_ref().unwrap()).unwrap()
+    );
+
+    assert_eq!(init_req_bytes, *init_req_msg.to_bytes().unwrap());
+
+    // Test partial values.
+    assert!(Message::from_bytes(&init_req_bytes[0..10]).unwrap().is_none());
+
+    // YAZ encodes true as 0x01, whereas rasn encodes it as 0xff.
+    let tag_12_result = 0xff;
+
+    // Bytes taking from a Yaz client init request
+    let init_resp_bytes = [
+        0xb5, 0x7f, 0x83, 0x02, 0x00, 0xe0, 0x84, 0x03, 0x00, 0xe9,
+        0x82, 0x85, 0x04, 0x04, 0x00, 0x00, 0x00, 0x86, 0x04, 0x04,
+        0x00, 0x00, 0x00, 0x8c, 0x01, tag_12_result, 0x9f, 0x6e, 0x05,
+        0x38, 0x31, 0x2f, 0x38, 0x31, 0x9f, 0x6f, 0x25, 0x53, 0x69,
+        0x6d, 0x70, 0x6c, 0x65, 0x32, 0x5a, 0x4f, 0x4f, 0x4d, 0x20,
+        0x55, 0x6e, 0x69, 0x76, 0x65, 0x72, 0x73, 0x61, 0x6c, 0x20,
+        0x47, 0x61, 0x74, 0x65, 0x77, 0x61, 0x79, 0x2f, 0x47, 0x46,
+        0x53, 0x2f, 0x59, 0x41, 0x5a, 0x9f, 0x70, 0x34, 0x31, 0x2e,
+        0x30, 0x34, 0x2f, 0x35, 0x2e, 0x33, 0x31, 0x2e, 0x31, 0x20,
+        0x63, 0x33, 0x63, 0x65, 0x61, 0x38, 0x38, 0x31, 0x65, 0x33,
+        0x65, 0x37, 0x65, 0x38, 0x30, 0x62, 0x30, 0x36, 0x39, 0x64,
+        0x64, 0x64, 0x31, 0x34, 0x32, 0x39, 0x39, 0x39, 0x34, 0x65,
+        0x35, 0x38, 0x38, 0x34, 0x31, 0x61, 0x63, 0x62, 0x31, 0x34
+    ];
+
+    let init_resp_msg = Message::from_bytes(&init_resp_bytes).unwrap().unwrap();
+
+    let MessagePayload::InitializeResponse(init_resp) = &init_resp_msg.payload else {
+        panic!("Wrong message type parsed: {init_resp_msg:?}");
+    };
+
+    assert_eq!(
+        "Simple2ZOOM Universal Gateway/GFS/YAZ",
+        general_string_to_str(init_resp.implementation_name.as_ref().unwrap()).unwrap()
+    );
+
+    assert_eq!(init_resp_bytes, *init_resp_msg.to_bytes().unwrap());
+}
+
